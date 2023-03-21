854eb4dcd7f8cafee4827a6d2ad09650ec874dc5
Nano Stark <62142317+nguyentruongton@users.noreply.github.com>
2023-03-21 23:34:54 +0700

    Added views login and home

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -2,8 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./views/Login";
 import Detail from "./views/Detail";
 import NotFound from "./views/NotFound";
+import Home from "./views/Home";
 
 const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
     {
         path: "/login",
         element: <Login />,
---
+++ react/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    important: "#root",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                googleSansThin: ["GoogleSans-Thin", "Roboto", "san-serif"],
+                googleSansLight: ["GoogleSans-Light", "Roboto", "san-serif"],
+                googleSansRegular: [
+                    "GoogleSans-Regular",
+                    "Roboto",
+                    "san-serif",
+                ],
+                googleSansMedium: ["GoogleSans-Medium", "Roboto", "san-serif"],
+                googleSansBold: ["GoogleSans-Bold", "Roboto", "san-serif"],
+                googleSansBlack: ["GoogleSans-Black", "Roboto", "san-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
